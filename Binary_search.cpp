5171f1031e140bbe871af882d2685b11b788d503
skc7 <skc7893@hotmail.com>
2018-02-14 20:25:41 +0530

    Added Merge sort array logic

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -54,12 +54,88 @@ void InsertionSort(int a[],int len){
     }
 }
 
+void Insert(int A[], int start){
+    int j = start;
+    while(j > 0 && A[j] < A[j-1]){
+        swap(A,j,j-1);
+        j = j-1;
+    }
+}
+
+void InsertionSort_rec(int A[],int start, int n){
+    if(start > n-1){
+        return;
+    }
+    Insert(A,start);
+    InsertionSort_rec(A,start+1,n);
+    return;
+}
+
+void Merge(int A[], int l, int m, int r){
+
+    int n1 = m-l+1;
+    int n2 = r-m;
+    int i,j,k;
+    int L[n1], R[n2];
+    for(i=0;i<n1;i++){
+        L[i] = A[l+i];
+    }
+
+    for(j=0;j<n2;j++){
+        R[j] = A[m+1+j];
+    }
+
+    i=0;
+    j=0;
+    k=l;
+
+    while(i<n1 &&j <n2){
+        if(L[i] < R[j] ){
+            A[k] = L[i];
+            i++;
+        }
+
+        if(L[i] > R[j] ){
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i<n1){
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j<n2){
+        A[k] = R[j];
+        j++;
+        k++;
+    }
+
+
+}
+
+void MergeSort(int A[], int l, int r){
+
+    if(l < r){
+        int m = (l + r)/2;
+        MergeSort(A,l,m);
+        MergeSort(A,m+1,r);
+        Merge(A,l,m,r);
+    }
+    return;
+
+}
+
+
 int main(){
 
     int a[] = {8,7,6,5,4,3};
     int x = 6;
 
-    InsertionSort(a,6);
+    MergeSort(a,0,5);
 
 
     for(int i=0; i<6;i++){
