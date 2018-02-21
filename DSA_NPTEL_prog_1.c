939188b3850d8d66ce5a0ec21c98463ae5d0c56a
skc7 <skc7893@hotmail.com>
2018-02-21 19:05:21 +0530

    Update DSA_NPTEL_prog_1.c

--- DSA_NPTEL_prog_1.c
+++ DSA_NPTEL_prog_1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 int main(){
   int C,N,K;
   int ans;
@@ -14,9 +16,19 @@ int main(){
   
   for(int i=0;i<N;i++){
       scanf("%d %d",&s[i],&e[i]);
+   
       Array[i][0] = s[i];
-      Array[i][1] = e[i];      
+      Array[i][1] = e[i]; 
+    
+    if(s[i] > C || e[i] > C || s[i] < 0 || e[i] < 0){
+    
+    	printf("Bad\n");
+      	return 0;
+        }
   }
+  
+
+  
 /*
   for(int i=0;i<N;i++){
     for(int j=0;j<N;j++){
@@ -24,7 +36,9 @@ int main(){
     }
 }
 */
-int temp = 0;
+  
+  
+  int temp = 0;
 for(int i = 0; i < N; i++){
   for(int j = i+1; j< N; j++){
       if(Array[j][0] < Array[i][0]){
@@ -36,9 +50,27 @@ for(int i = 0; i < N; i++){
         Array[j][1] = Array[i][1];
         Array[i][1] = temp;
       }
+
+      if(Array[j][0] == Array[i][0]){
+          if(Array[j][1] < Array[i][1]){
+            temp = Array[j][0];
+            Array[j][0] = Array[i][0];
+            Array[i][0] = temp;
           
+            temp = Array[j][1];
+            Array[j][1] = Array[i][1];
+            Array[i][1] = temp;
+
+          }
+        
+      }
+  
     }
 }
+  
+
+ //MergeSort(Array,0,N-1);
+  
 /*
 for(int i=0;i<N;i++){
     for(int j=0;j<N;j++){
@@ -79,11 +111,21 @@ if(K == 1){
         ans = 0;
         
     }
+  
+  else if(error == 0){
+        ans = 1;
+    }
 
     else{
         for(int i=0;i<=N;i++){
             
                 if(remaining[i] < 0 && remaining[i+1] >= 0){
+                  
+                  	if(error == 2){
+                        ans = 0;
+                        break;
+                    }
+                  
                     if(remaining[i-1] + remaining[i] + (Array[i-1][1] - Array[i-1][0] +1) >= (Array[i-1][1] - Array[i-1][0] +1)){
                         ans = 1;
                         break;
@@ -91,6 +133,15 @@ if(K == 1){
                     if(remaining[i] + remaining[i+1] + (Array[i][1] - Array[i][0] +1) >= (Array[i][1] - Array[i][0] +1)){
                         ans = 1;
                         break;
+                    }
+                  	
+                  else{
+                        for(int j = 0; j <= N; j++){
+                            if(Array[i-1][1] - Array[i-1][0] +1 <= remaining[j]){
+                                ans = 1;
+                                break;
+                            }
+                        }
                     }
                     
                 }
