365b9cddfb9a07511fb3ab482b528480c228e37a
skc7 <skc7893@hotmail.com>
2018-03-04 12:30:32 +0530

    Added flyod_walsh algorithm for min distance among all nodes in DAG

--- CPP_graph_matrix.cpp
+++ CPP_graph_matrix.cpp
@@ -29,10 +29,12 @@ struct Graph* createGraph(int V){
     return G;
 }
 
-void addEdge(struct Graph* G, int u, int v){
+void addEdge(struct Graph* G, int u, int v,int** weight,int w){
     struct Graph* g = G;
     g->Adj[u][v] = 1;
     g->Adj[v][u] = 1;
+    weight[src][dest] = w;
+    //weight[dest][src] = w;
 }
 
 void printGraph(struct Graph* graph){
@@ -45,18 +47,50 @@ void printGraph(struct Graph* graph){
     }
 }
 
+void Flyod_walsh(struct Graph* G,int** weight){
+    int w[G->V][G->V][G->V];
+    for(int i=0;i<G->V;i++){
+        for(int j=0;j<G->V;j++){
+            w[i][j][0] = 9999;
+        }
+    }
+
+    for(i=0;i<G->V;i++){
+        for(j=0;j<G->V;j++){
+            w[i][j][0] = weight[i][j];
+        }
+    }
+
+    for(int k=1;k<G->V;k++){
+        w[i][j][k] = min(w[i][j][k-1], w[i][k][k-1] +w[k][j][k-1] );
+    }
+
+}
 
 int main(){
 
     int V=5;
     struct Graph* graph = createGraph(V);
-    addEdge(graph,0,1);
-    addEdge(graph, 0, 4);
-    addEdge(graph, 1, 2);
-    addEdge(graph, 1, 3);
-    addEdge(graph, 1, 4);
-    addEdge(graph, 2, 3);
-    addEdge(graph, 3, 4);
+
+    int** weight = (int**)malloc(V * sizeof(int*));
+    for(int i=0;i<V;i++){
+        weight[i] = (int*)malloc(V * sizeof(int));
+    }
+
+    for(int i=0;i<G->V;i++){
+        for(int j=0;j<G->V;j++){
+            weight[i][j] = 9999;
+        }
+    }
+
+
+    addEdge(graph,0,1,weight,1);
+    addEdge(graph, 0, 4,weight,1);
+    addEdge(graph, 1, 2,weight,1);
+    addEdge(graph, 1, 3,weight,1);
+    addEdge(graph, 1, 4,weight,1);
+    addEdge(graph, 2, 3,weight,1);
+    addEdge(graph, 3, 4,weight,1);
     printGraph(graph);
     return 0;
 
